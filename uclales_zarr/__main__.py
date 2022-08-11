568a8f42a271fad4b175bc3b64e7cdf6e7fb5633
Leif Denby <leif@denby.eu>
2022-08-11 11:17:20 +0100

    add experiment name to cli

--- uclales_zarr/__main__.py
+++ uclales_zarr/__main__.py
@@ -7,9 +7,10 @@ if __name__ == "__main__":
 
     argparser = argparse.ArgumentParser()
     argparser.add_argument("source_data_path", type=Path)
+    argparser.add_argument("experiment_name")
     args = argparser.parse_args()
 
     import ipdb
 
     with ipdb.launch_ipdb_on_exception():
-        main(fp_source_data=args.source_data_path, exp_name="rico_gcss")
+        main(fp_source_data=args.source_data_path, exp_name=args.experiment_name)
