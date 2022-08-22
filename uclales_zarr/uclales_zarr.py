c12643967084835f6f9fff4e148ec39cfdae5ab9
Leif Denby <leif@denby.eu>
2022-08-22 07:52:06 +0100

    support for native use of netcdf3

--- uclales_zarr/uclales_zarr.py
+++ uclales_zarr/uclales_zarr.py
@@ -7,11 +7,13 @@ import dask
 import fsspec
 import fsspec.implementations
 import fsspec.implementations.dirfs
+import h5py
 import ujson  # fast json
 import xarray as xr
 from dask.diagnostics import ProgressBar
 from kerchunk.combine import MultiZarrToZarr
 from kerchunk.hdf import SingleHdf5ToZarr
+from kerchunk.netCDF3 import NetCDF3ToZarr
 from loguru import logger
 from tqdm.auto import tqdm
 
@@ -34,8 +36,8 @@ def _convert_nc3_to_nc4(fps_src, dest_path):
     return files_nc4
 
 
-def _create_singlefile_zarr_jsons(path_src_jsons, fps_nc4_files):
-    logger.info("Creating JSON file for each individual source NetCDF4 file")
+def _create_singlefile_zarr_jsons(path_src_jsons, fps_nc_files, is_netcdf4=False):
+    logger.info("Creating JSON file for each individual source NetCDF file")
     path_src_jsons = Path(path_src_jsons)
     path_src_jsons.mkdir(exist_ok=True, parents=True)
     fs = fsspec.implementations.dirfs.DirFileSystem(
@@ -45,13 +47,16 @@ def _create_singlefile_zarr_jsons(path_src_jsons, fps_nc4_files):
 
     def gen_json(fp):
         with fsspec.open(fp) as infile:
-            h5chunks = SingleHdf5ToZarr(infile, str(fp), inline_threshold=300)
+            if is_netcdf4:
+                h5chunks = SingleHdf5ToZarr(infile, str(fp), inline_threshold=300)
+            else:
+                h5chunks = NetCDF3ToZarr(str(fp))
             outf = f"{fp.name}.json"
             with fs.open(outf, "wb") as f:
                 f.write(ujson.dumps(h5chunks.translate()).encode())
             return Path(fs.path) / outf
 
-    files = fps_nc4_files
+    files = fps_nc_files
     json_single_fps = dask.compute(
         *[dask.delayed(gen_json)(u) for u in files], retries=10
     )
@@ -97,17 +102,55 @@ def _read_multizarr(rpath):
     return ds
 
 
-def main(fp_source_data, exp_name):
-    fp_source_data = Path(fp_source_data)
-    fp_dest_data = fp_source_data.parent / (fp_source_data.name + "__zarr")
+def _find_source_files(fp_source_data, data_kind, exp_name):
+    if data_kind == "3d":
+        fps_src = list(fp_source_data.glob(f"{exp_name}.????????.nc"))
+    elif data_kind == "3d__first_10x10":
+        fps_src = list(fp_source_data.glob(f"{exp_name}.000?000?.nc"))
+    elif data_kind in ["xy", "xz", "yz"]:
+        fps_src = list(fp_source_data.glob(f"{exp_name}.out.{data_kind}.????.????.nc"))
+    elif data_kind == "xy__first_10x10":
+        fps_src = list(fp_source_data.glob(f"{exp_name}.out.xy.000?.000?.nc"))
+    else:
+        raise NotImplementedError(data_kind)
 
-    fps_src = list(fp_source_data.glob(f"{exp_name}.000?000?.nc"))
     logger.info(f"Found {len(fps_src)} soure files")
 
-    fps_nc4_files = _convert_nc3_to_nc4(fps_src=fps_src, dest_path=fp_dest_data / "nc4")
-    fps_src_jsons = _create_singlefile_zarr_jsons(
-        path_src_jsons=fp_dest_data / "src_jsons", fps_nc4_files=fps_nc4_files
+    return fps_src
+
+
+def main(fp_source_data, exp_name, data_kind, convert_to_nc4=False):
+    fp_source_data = Path(fp_source_data, data_kind=data_kind)
+    fp_dest_data = fp_source_data.parent / (fp_source_data.name + "__zarr")
+
+    fps_src = _find_source_files(
+        fp_source_data=fp_source_data, data_kind=data_kind, exp_name=exp_name
     )
+
+    dirname_src_jsons = f"src_jsons__{data_kind}"
+    if convert_to_nc4:
+        dirname_src_jsons += "_nc4"
+
+    fp_src_jsons = fp_dest_data / dirname_src_jsons
+    if convert_to_nc4:
+        fps_src_ready = _convert_nc3_to_nc4(
+            fps_src=fps_src, dest_path=fp_dest_data / f"{data_kind}_nc4"
+        )
+    else:
+        fps_src_ready = fps_src
+
+    try:
+        h5py.File(fps_src[0], "r")
+        is_netcdf4 = True
+    except OSError:
+        is_netcdf4 = False
+
+    fps_src_jsons = _create_singlefile_zarr_jsons(
+        path_src_jsons=fp_src_jsons,
+        fps_nc_files=fps_src_ready,
+        is_netcdf4=is_netcdf4,
+    )
+
     # check we can open one of the single-file jsons without issue
     _open_single_json(filepath=fps_src_jsons[0])
 
